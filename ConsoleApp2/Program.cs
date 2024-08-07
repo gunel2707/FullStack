db3aea58f0c5ba1d410a23f4c9e677d22c42b6b3
gunel2707 <gunel.muradova2707@gmail.com>
2024-08-07 09:28:46 +0400

    'first'

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please,enter first number");
+            int var1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please,enter second number");
+            int var2 = int.Parse(Console.ReadLine());
+            var result = Sum(var1, var2);
+            Console.WriteLine("result:" + result.ToString());
+
+        }
+        private static int Sum(int x, int y)
+        {
+            return x + y;
+                }
+    }
+}
